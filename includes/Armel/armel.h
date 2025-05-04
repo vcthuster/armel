a78ca23a044aa0b4cc0da790208fb0bfad4745f2
vcthuster <remorque.fruit.7a@icloud.com>
2025-05-04 13:47:32 +0200

    Messing up with CI workflows

--- includes/Armel/armel.h
+++ includes/Armel/armel.h
@@ -34,10 +34,10 @@
 #ifndef ARMEL_H
 #define ARMEL_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 #include <assert.h>
